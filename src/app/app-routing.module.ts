03d004fda6a563921b97e1512093325cfb59d27d
Israel Belete <ibelete2000@gmail.com>
2021-05-23 20:47:52 -0600

    header, section, footer changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SectionComponent } from './section/section.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: SectionComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
