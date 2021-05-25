71ce1b2df4daa27bf86eabbcd549fc5e949e61e8
Israel Belete <ibelete2000@gmail.com>
2021-05-24 21:44:49 -0600

    contact page created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutPageComponent } from './about-page/about-page.component';
+import { ContactPageComponent } from './contact-page/contact-page.component';
 import { SectionComponent } from './section/section.component';
 
 const routes: Routes = [
-  { path: '', component: SectionComponent }
+  { path: '', component: SectionComponent },
+  { path: 'Contact', component: ContactPageComponent },
+  { path: 'About', component: AboutPageComponent },
+
 ];
 
 @NgModule({
