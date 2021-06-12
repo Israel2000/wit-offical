e6b17a43e499e1fabdbb8c6e6c22cd9316cf2995
Israel Belete <ibelete2000@gmail.com>
2021-06-11 22:11:03 -0600

    ng half navigations completed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,8 @@ import { WorkDetailsComponent } from './work-details/work-details.component';
 
 const routes: Routes = [
   { path: '', component: SectionComponent },
+  // { path: '/home', component: SectionComponent },
+
   { path: 'Contact', component: ContactPageComponent },
   { path: 'About', component: AboutPageComponent },
   { path: 'work', component: WorkDetailsComponent }
