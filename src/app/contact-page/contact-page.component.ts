22c87f73f9467df2ad68009f36c4275aaed7512a
Rishabh_Sapkal <rishabhsapkal007@gmail.com>
2021-06-20 18:49:01 +0530

    Contact Form Linked and navigation adjustment made

--- src/app/contact-page/contact-page.component.ts
+++ src/app/contact-page/contact-page.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { DbServiceService } from '../shared/db-service.service';
+import { ToastrService } from 'ngx-toastr';
 @Component({
   selector: 'app-contact-page',
   templateUrl: './contact-page.component.html',
@@ -7,9 +9,65 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactPageComponent implements OnInit {
 
-  constructor() { }
+  public contactsForm: FormGroup;  // Define FormGroup to student's form
+
+  constructor(
+    public crudApi: DbServiceService,  // CRUD API services
+    public fb: FormBuilder,       // Form Builder service for Reactive forms
+    public toastr: ToastrService,  // Toastr service for alert message
+  ) { }
 
   ngOnInit(): void {
+
+    this.crudApi.GetContactsList();  // Call GetStudentsList() before main form is being called
+    this.contactForm()
+  }
+  contactForm() {
+    this.contactsForm = this.fb.group({
+      name: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(25)]],
+      email: ['', [Validators.required, Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$'), Validators.maxLength(50)]],
+      mobileNumber: ['', [Validators.required, Validators.pattern('^[0-9]+$'), Validators.maxLength(15)]],
+      message: ['', Validators.maxLength(200)]
+    })
   }
 
+  // Accessing form control using getters
+  get name() {
+    return this.contactsForm.get('name');
+  }
+
+
+  get email() {
+    return this.contactsForm.get('email');
+  }
+
+  get mobileNumber() {
+    return this.contactsForm.get('mobileNumber');
+  }
+
+
+  get message() {
+    return this.contactsForm.get('message');
+  }
+
+  // Reset student form's values
+  ResetForm() {
+    this.contactsForm.reset();
+  }
+
+
+  submitContactsData(data) {
+
+    console.log(data.valid)
+
+    if (data.valid) {
+      this.crudApi.AddContactUs(this.contactsForm.value); // Submit student data using CRUD API
+      this.toastr.success('Hi, ' + this.contactsForm.controls['name'].value + '. We will contact you shortly.'); // Show success message when data is successfully submited
+      this.ResetForm();  // Reset form when clicked on reset button
+    }
+    else {
+      this.toastr.error('Please Fill the Mandatory Fields')
+    }
+
+  };
 }
